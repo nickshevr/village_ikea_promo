e8db074a4f3caa0ebdc5380a9b1e5bd9d9476856
nickshevr <nickshevr@gmail.com>
2021-05-20 21:37:03 +0300

    Mobile version and script

---
+++ src/components/FinalMobile/index.js
@@ -0,0 +1,41 @@
+import React, {useContext} from 'react';
+
+import {
+  GlobalStateContext,
+} from "../../context/GlobalContextProvider"
+import result_1 from '../../images/Result/result_1.png';
+import result_2 from '../../images/Result/result_2.png';
+import result_3 from '../../images/Result/result_3.png';
+import result_4 from '../../images/Result/result_4.png';
+
+import * as styles from './styles.module.css';
+
+const disabledViewStyle = {
+    display: 'none',
+  };
+  
+const noStyle = {};
+
+const Image = ({src, style}) => (
+    <img
+        style={style}
+        src={src}
+        className={styles.image}
+    />
+);
+
+const Final = ({style}) => {
+    const state = useContext(GlobalStateContext);
+    const profile = Object.keys(state).sort((a, b) => state[a] - state[b] > 0 ? -1 : 1);
+
+    return (
+        <div style={style}>
+            <Image src={result_1} style={profile[1] === "1" ? noStyle : disabledViewStyle}/>
+            <Image src={result_2} style={profile[1] === "2" ? noStyle : disabledViewStyle}/>
+            <Image src={result_3} style={profile[1] === "3" ? noStyle : disabledViewStyle}/>
+            <Image src={result_4} style={profile[1] === "4" ? noStyle : disabledViewStyle}/>
+        </div>
+    )
+};
+
+export default Final;
---
+++ src/pages/mobile.js
@@ -0,0 +1,39 @@
+import React, {useContext} from "react"
+import {
+  GlobalStateContext,
+} from "../context/GlobalContextProvider"
+import Pool from '../components/Pool';
+import FinalMobile from '../components/FinalMobile';
+import {
+  FIRST_IMAGES,
+  SECOND_IMAGES,
+  THIRD_IMAGES,
+  FOURTH_IMAGES,
+  FITH_IMAGES,
+} from '../data/images';
+
+import './styles.css'
+
+const disabledViewStyle = {
+  display: 'none',
+};
+
+const noStyle = {};
+
+const IndexPage = () => {
+  const state = useContext(GlobalStateContext);
+
+  return (
+    <div id="main">
+      <title>Village IKEA Promo</title>
+      <Pool images={FIRST_IMAGES} style={state.currentStep === 1 ? noStyle : disabledViewStyle} />
+      <Pool images={SECOND_IMAGES} style={state.currentStep === 2 ? noStyle : disabledViewStyle} />
+      <Pool images={THIRD_IMAGES} style={state.currentStep === 3 ? noStyle : disabledViewStyle} />
+      <Pool images={FOURTH_IMAGES} style={state.currentStep === 4 ? noStyle : disabledViewStyle} />
+      <Pool images={FITH_IMAGES} style={state.currentStep === 5 ? noStyle : disabledViewStyle} />
+      <FinalMobile style={state.currentStep === 6 ? noStyle : disabledViewStyle} />
+    </div>
+  )
+}
+
+export default IndexPage
